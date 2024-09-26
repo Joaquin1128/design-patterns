684566f95c8e98124f25efbbc6f49a4ffa3cb20b
Joaquin1128 <Joaquin.augusto@hotmail.com>
2024-09-26 08:59:55 -0300

    first commit

---
+++ src/main/java/com/headfirst/design/patterns/DesignPatterns.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.headfirst.design.patterns;
+
+/**
+ *
+ * @author Joaquin
+ */
+public class DesignPatterns {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
